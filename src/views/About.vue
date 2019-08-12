295ca97d568e4fa624baa1e3fd42aae131f6405d
vineetkoppalkar <koppalkarvineet@gmail.com>
2019-08-12 09:58:52 -0400

    Setup a todo list application using vuejs

---
+++ src/views/About.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>
+      Exercitation cillum ea sint consequat qui sit labore laborum veniam ullamco culpa. Anim enim do mollit nulla. Exercitation Lorem commodo sit quis sunt in excepteur adipisicing velit eu consequat aliqua non. Duis non consectetur eu incididunt cupidatat ullamco ex excepteur est sint adipisicing in dolore. Enim cupidatat quis fugiat aliquip culpa velit enim tempor eiusmod reprehenderit consectetur non Lorem qui. Ad deserunt sit sunt esse et tempor minim nisi aliqua aute.
+    </p>
+  </div>
+</template>
